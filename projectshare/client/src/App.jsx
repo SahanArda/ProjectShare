fc085f8f459b5152b03ddf26ce29cb13e2518af6
Arda Sahan <ardambp@outlook.com>
2023-11-20 12:34:18 +0000

    creating frontend

--- projectshare/client/src/App.jsx
+++ projectshare/client/src/App.jsx
@@ -1,7 +1,35 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
+import HomePage from "scenes/homePage/homePage.jsx";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings[mode]), [mode]);
+  return (
+    <div className="app">
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
