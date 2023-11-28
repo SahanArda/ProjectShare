19448b80f0078f2d0f44dbc68829540beda57fb9
Arda Sahan <ardambp@outlook.com>
2023-11-28 21:32:51 +0000

    created profile widget for home page

--- client/src/pages/homePage/homePage.jsx
+++ client/src/pages/homePage/homePage.jsx
@@ -1,11 +1,27 @@
-import { Box } from '@mui/material'
-import Navbar from '../navbar/navbar';
+import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "../navbar/navbar";
+import { useSelector } from "react-redux";
+import UserWidget from "../widgets/UserWidget";
+
 const HomePage = () => {
-    return (
-        <Box>
-            <Navbar />
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
-    );
-}
+      </Box>
+    </Box>
+  );
+};
 
 export default HomePage;
