fff4c35d027de35d7ac3213105a0b2c4052d2b63
Arda Sahan <ardambp@outlook.com>
2023-11-28 16:18:05 +0000

    added dark/light mode button to login and register pages added comments and added auth to routes to prevent unauthorized users from accessing the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/homePage/homePage.jsx";
 import LoginPage from "./pages/loginPage/loginPage.jsx";
 import ProfilePage from "./pages/profilePage/profilePage.jsx";
@@ -13,6 +13,9 @@ function App() {
   const mode = useSelector((state) => state.mode);
   // useMemo here is used to ensure that the theme is only rendered once the mode changes and not every time the component re-renders
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  // This is going to be used to check if the token exists for the user and if it does then the user is authorized to access the home page
+  // By adding this to the routes such as the homePage it will ensure non-authorized users cannot access the page
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -22,8 +25,14 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />} 
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
             {/* <Route path="*" element={<Error404 />} /> */}
           </Routes>
         </ThemeProvider>
